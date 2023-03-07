64dbccd494b14306266e2de1ae97388777facb81
MA Shanpeng <shanpeng.ma@gmail.com>
2023-03-07 14:34:34 +0900

    Update vehicle_info.hpp

--- include/zmp/vehicle_info.hpp
+++ include/zmp/vehicle_info.hpp
@@ -141,5 +141,67 @@ static inline double KmhToMs(double v)
     return (v * 1000.0 / (60.0 * 60.0));
 }
 
-#define _STEERING_ANGLE_ERROR 0 //deg
+#define _STEERING_ANGLE_ERROR 0 // deg
+
+// accel/brake parameters
+// 20 km/h 以下的速度下 PID 控制的比例、积分和微分参数。
+#define _K_ACCEL_P_UNTIL20 40.0 // 30.0
+#define _K_ACCEL_I_UNTIL20 0.1  // 5.0
+#define _K_ACCEL_D_UNTIL20 0.1
+// 10 km/h 以下的速度下 PID 控制的比例、积分和微分参数。
+#define _K_ACCEL_P_UNTIL10 40.0 // 30.0
+#define _K_ACCEL_I_UNTIL10 0.1  // 5.0
+#define _K_ACCEL_D_UNTIL10 0.1
+// 积分限幅的周期数。
+#define _K_ACCEL_I_CYCLES 1000
+// 积分项的最大值。
+#define _ACCEL_MAX_I 3000
+// 两次控制间加速踏板的最大变化量。
+#define _ACCEL_STROKE_DELTA_MAX 1000
+// 松开加速踏板时每次控制间踏板位置的最大变化量。
+#define _ACCEL_RELEASE_STEP 400
+// 加速踏板的最大位置值。
+#define _ACCEL_PEDAL_MAX 1700
+// 加速踏板位置的偏移值。
+#define _ACCEL_PEDAL_OFFSET 200
+
+#define _K_BRAKE_P 40.0
+#define _K_BRAKE_I 10.0
+#define _K_BRAKE_D 10.0
+// 积分限幅的周期数。
+#define _K_BRAKE_I_CYCLES 100
+#define _BRAKE_MAX_I 500               // 200 是积分项的最大值，用于限制积分项对控制器输出的影响。
+#define _BRAKE_STROKE_DELTA_MAX 1000   // 刹车踏板位移的最大变化量，用于限制刹车踏板输出的变化速度。
+#define _BRAKE_RELEASE_STEP 500        // 刹车踏板释放时每次变化的步长。
+#define _BRAKE_PEDAL_MAX 3500          // 4095 //刹车踏板的最大输出值。
+#define _BRAKE_PEDAL_MED 3000          // 刹车踏板在制动中等力度下的输出值。
+#define _BRAKE_PEDAL_STOPPING_MAX 2500 // 急停车时刹车踏板的最大输出值。
+#define _BRAKE_PEDAL_STOPPING_MED 1500 // 急停车时刹车踏板的中等输出值。
+// 刹车踏板偏移量
+#define _BRAKE_PEDAL_OFFSET 1000
+
+// steering parameters
+#define _STEERING_MAX_ANGVELSUM 1000    // 方向盘角速度和的最大值
+#define _STEERING_MAX_TORQUE 1800       // 2000 方向盘扭矩最大值
+#define _STEERING_MAX_SUM 3000          // 方向盘偏差累加和的最大值
+#define _STEERING_ANGVEL_BOUNDARY 500.0 // deg/s //角速度边界
+#define _STEERING_IGNORE_ERROR 0.5      // 1.0 // deg //忽略的方向盘偏差
+#define _STEERING_DIFF_SMALL 10         // deg //方向盘偏差小于该值时使用更严格的PID系数
+
+// fast PID factors
+#define _K_STEERING_P 8.0 // 用于计算方向盘扭矩的PID系数
+#define _K_STEERING_I 0.8
+#define _K_STEERING_D 1.5
+
+// fast PID factors
+#define _K_STEERING_P_STRICT 8.0 // 60
+#define _K_STEERING_I_STRICT 0.8 // 5.0
+#define _K_STEERING_D_STRICT 1.5 // 5.0
+
+// slow PID factors
+// D = 1.7 might also work.
+#define _K_STEERING_P_SLOW 8.0
+#define _K_STEERING_I_SLOW 0.8
+#define _K_STEERING_D_SLOW 1.5
+
 #endif // vehicle_info_hpp
